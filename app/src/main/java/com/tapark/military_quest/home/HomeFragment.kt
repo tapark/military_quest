416e89bac75412ced18809fee4b7136144870656
박태규 <tk.park@autowini.com>
2023-01-11 18:36:53 +0900

    progress_view

--- app/src/main/java/com/tapark/military_quest/home/HomeFragment.kt
+++ app/src/main/java/com/tapark/military_quest/home/HomeFragment.kt
@@ -3,7 +3,9 @@ package com.tapark.military_quest.home
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.widget.TextView
 import androidx.fragment.app.viewModels
+import com.skydoves.progressview.ProgressView
 import com.tapark.military_quest.MainActivity
 import com.tapark.military_quest.R
 import com.tapark.military_quest.base.BaseFragment
@@ -35,41 +37,6 @@ class HomeFragment: BaseFragment<FragmentHomeBinding, HomeViewModel>() {
 
         initData()
         onClick()
-
-        val userInfo = PrefManager.getUserInfo()
-
-        val enterSec = (ymdToMilli(userInfo.enter.value) / 1000).toInt()
-        val retireSec = (ymdToMilli(userInfo.retire.value) / 1000).toInt()
-        val currentSec = (System.currentTimeMillis() / 1000).toInt()
-
-        viewDataBinding.backgroundWaveLayout.apply {
-            max = retireSec - enterSec
-            progress = currentSec - enterSec
-        }
-
-        val timeCurrent = System.currentTimeMillis() - ymdToMilli(userInfo.enter.value)
-        val timeLeft = ymdToMilli(userInfo.retire.value) - System.currentTimeMillis()
-        val timeTotal = ymdToMilli(userInfo.retire.value) - ymdToMilli(userInfo.enter.value)
-
-        val currentPercent = timeCurrent.toDouble() / timeTotal.toDouble() * 100
-        val leftPercent = timeLeft.toDouble() / timeTotal.toDouble() * 100
-
-        Log.d("박태규", "$currentPercent / $leftPercent")
-
-        val countDownTimer = object : CountDownTimer(timeLeft,100) {
-
-            override fun onTick(p0: Long) {
-//                Log.d("박태규", "tic : $p0 / $timeLeft")
-
-                val percent = (timeLeft - p0).toDouble() / timeLeft.toDouble() * leftPercent + currentPercent
-                viewDataBinding.waveText.text = String.format("%.7f", percent)
-            }
-
-            override fun onFinish() {
-
-            }
-
-        }.start()
     }
 
     private fun onClick() {
@@ -82,13 +49,41 @@ class HomeFragment: BaseFragment<FragmentHomeBinding, HomeViewModel>() {
 
         userInfo = PrefManager.getUserInfo()
 
+        // profile data
         viewDataBinding.apply {
             subNameText.text = getString(R.string.sub_name_form, userInfo.company.value, userInfo.rank.value)
-            nameText.text = userInfo.name.value.ifEmpty { "강한용사" }
+            nameText.text = userInfo.name.value.ifEmpty { "김너굴" }
             enterText.text = getString(R.string.enter_date_form, userInfo.enter.value)
             retireText.text = getString(R.string.retire_date_form, userInfo.retire.value)
             profileImageView.setImageBitmap(PrefManager.getBitmap(KEY_PROFILE_BITMAP))
         }
+
+        // main quest
+        initMainProgress(userInfo.enter.value, userInfo.retire.value)
+
+    }
+
+    private fun initMainProgress(startDate: String, endDate: String) {
+
+        val startMilli = ymdToMilli(startDate)
+        val endMilli = ymdToMilli(endDate)
+        val currentMilli = System.currentTimeMillis()
+        val dDay = (endMilli - currentMilli) / (24 * 60 * 60 * 1000)
+
+        val currentPercent = (currentMilli - startMilli).toFloat() / (endMilli - startMilli).toFloat() * 100
+        val leftPercent = 100 - currentPercent
+
+        viewDataBinding.dDayText.text = getString(R.string.d_day_form, dDay.toInt())
+        viewDataBinding.endDateText.text = endDate
+        viewDataBinding.mainProgressView.progress = currentPercent
+
+        object : CountDownTimer(endMilli - currentMilli, 100) {
+            override fun onTick(p0: Long) {
+                val percent = (endMilli - currentMilli - p0).toDouble() / (endMilli - currentMilli).toDouble() * leftPercent + currentPercent
+                viewDataBinding.progressText.text = String.format("%.7f", percent)
+            }
+            override fun onFinish() {  }
+        }.start()
 
     }
 
