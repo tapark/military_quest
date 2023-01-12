5be5ad80abb3b3d12d855b66ee0b0d62aa0c9c80
박태규 <tk.park@autowini.com>
2023-01-12 19:21:07 +0900

    sub_quest_adapter

---
+++ app/src/main/java/com/tapark/military_quest/home/adapter/SubQuestAdapter.kt
@@ -0,0 +1,25 @@
+package com.tapark.military_quest.home.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tapark.military_quest.databinding.ItemSubQuestBinding
+
+class SubQuestAdapter: RecyclerView.Adapter<SubQuestAdapter.SubQuestViewHolder>() {
+
+  inner class SubQuestViewHolder(val binding: ItemSubQuestBinding): RecyclerView.ViewHolder(binding.root) {
+
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubQuestViewHolder {
+
+  }
+
+  override fun onBindViewHolder(holder: SubQuestViewHolder, position: Int) {
+
+  }
+
+  override fun getItemCount(): Int {
+
+  }
+
+}
